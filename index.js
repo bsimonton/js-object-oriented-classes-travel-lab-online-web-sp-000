d604dadb871366fe4f7a32adba4534da0e3f52aa
Brian Simonton <bsimonton78@gmail.com>
2019-07-11 03:36:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,9 +30,16 @@ class Route {
   
 constructor(beginningLocation, endingLocation){
   
-  
+  this.beginningLocation = beginningLocation;
+  this.endingLocation = endingLocation;
   
 }
+
+
+
+
+
+
 }
 
 
